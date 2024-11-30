02fac45122eb46aa76a94e7b0c5f194ab6997c77
denis.islamov <1@2fw.ru>
2024-12-01 01:27:26 +0500

    add YANDEX GPT Lite ai

--- helpdeck_project/tasks/views.py
+++ helpdeck_project/tasks/views.py
@@ -1,9 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Task
+from .models import TypeTask
 from django.contrib.auth.models import User
 
 # Create your views here.
+def yandex_gpt(text_for_user):
+    import requests
+    prompt = {
+    "modelUri": "gpt://b1gkb4guem02kb4oa39o/yandexgpt-lite",
+    "completionOptions": {
+        "stream": False,
+        "temperature": 0.6,
+        "maxTokens": "2000"
+    },
+    "messages": [
+        {
+            "role": "system",
+            "text": "Ты ассистент секретаря, который принимает заявки в службе компьютерной технической поддержки."
+        },
+        {
+            "role": "user",
+            "text": "Привет, Секретарь! Прочитай обращение пользователя и определи к какой категории из трех указанных (1.обслуживание (обновления, техподдержка и т.д.), 2.инциденты (неполадки системы или оборудования критически препятствующие работе), 3. запросы на изменение состояния (установка нового оборудования или ПО)) ты бы отнес следующее обращение. Ответь одной цифрой - порядковым номером категории"
+        },
+        {
+            "role": "assistant",
+            "text": "Привет! Чтобы обрабатывать тексты, тебе нужно знать русский язык и природу человека. Начнем с изучения основных обращений в тех.поддержку."
+        },
+        {
+            "role": "user",
+            "text": text_for_user
+        }
+                ]
+    }
+    url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+    headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Api-Key AQVNxBeG9c3sQp7qXi5qUSrTAArNxxKypW8gOCov"
+    }
+    response = requests.post(url, headers=headers, json=prompt)
+    result = response.text
+    return ''.join(filter(str.isdigit, result))[0]
+
 
 def index(request):
     return render(request, 'tasks\index.html')
@@ -15,10 +53,13 @@ def task_info(request, task_id):
 def new_task(request, email=None):
     if request.method == 'POST':
         new_task_form = Task()
+        id_type_task = yandex_gpt(request.POST['message'])
+        print(id_type_task)
         new_task_form.title = request.POST['title'] if request.POST['title'] else ''
         new_task_form.message = request.POST['message'] if request.POST['message'] else ''
         new_task_form.authors = request.POST['authors_email'] if request.POST['authors_email'] else ''
         new_task_form.contractor = User.objects.get(id=1)
+        new_task_form.typetask = TypeTask.objects.get(id=id_type_task)
         new_task_form.closed = False
         new_task_form.save()
         return render(request,
