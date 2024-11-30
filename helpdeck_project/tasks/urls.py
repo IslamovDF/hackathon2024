5952c24fdbe4353ad2f161b714b7f298db27f4ae
denis.islamov <1@2fw.ru>
2024-11-30 19:27:07 +0500

    add param in url

--- helpdeck_project/tasks/urls.py
+++ helpdeck_project/tasks/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("new/", views.new_task, name="new_task"),
+    path("new/<str:email>", views.new_task, name="new_task"),
     path('tasks/', views.tasks_list, name='list'),
     path('dashboard/', views.statistics, name='statistics'),
     path("about/", views.about, name="about"),
--- helpdeck_project/tasks/views.py
+++ helpdeck_project/tasks/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 def index(request):
     return render(request, 'tasks\index.html')
 
-def new_task(request):
+def new_task(request, email=None):
     if request.method == 'POST':
         new_task_form = Task()
         new_task_form.title = request.POST['title'] if request.POST['title'] else ''
@@ -20,7 +20,7 @@ def new_task(request):
                       'tasks/success_task.html',
                       {'task_id': new_task_form.id,
                         'message_text': new_task_form.message})
-    return render(request, 'tasks/new_task.html')
+    return render(request, 'tasks/new_task.html', {'mail': email if email else ''})
 
 def tasks_list(request):
     tasks_list_to_page = Task.objects.all()
